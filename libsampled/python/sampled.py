a4873b221596fb9972af6c98409c0591920e99cb
Simon Mirco <simon@box.id.au>
2022-02-15 23:23:33 -0800

    python library thread-safety

--- libsampled/python/sampled.py
+++ libsampled/python/sampled.py
@@ -5,80 +5,100 @@ import threading
 import socket
 import os
 import json
+import queue
 
 # flush sample buffer to daemon when either size or interval is reached
-SAMPLED_BUFFER_FLUSH_SIZE = int(os.getenv("SAMPLED_BUFFER_FLUSH_SIZE", 10))
+SAMPLED_BUFFER_FLUSH_SIZE = int(os.getenv("SAMPLED_BUFFER_FLUSH_SIZE", 100))
 SAMPLED_BUFFER_FLUSH_INTERVAL_MS = int(
     os.getenv("SAMPLED_BUFFER_FLUSH_INTERVAL_MS", 1000)
 )
 
 SAMPLED_DAEMON_HOST = os.getenv("SAMPLED_DAEMON_HOST", "localhost")
 SAMPLED_DAEMON_PORT = os.getenv("SAMPLED_DAEMON_PORT", 7675)
-SAMPLED_MESSAGE_SEPARATOR = "\x00"
 
 FieldType = Union[str, float, int, List[str]]
+
 
 def _now_milliseconds():
     return int(time.time() * 1000)
 
 
 class SampleBuffer:
-    __buffer = []
-    __last_flush = _now_milliseconds()
-
     def __init__(self):
-        _sampled_thread = threading.Thread(
-            target=self._flush_loop,
-            name="SampledPublisher",
-            daemon=True
+        self.__q = queue.Queue(maxsize=1000)
+        self.__buffer = b""
+        self.__buffer_message_count = 0
+        self.__last_flush = _now_milliseconds()
+        self.__socket = None
+
+        sampled_thread = threading.Thread(
+            target=self._flush_loop, name="SampledPublisher", daemon=True
         )
-        _sampled_thread.start()
-        
+        sampled_thread.start()
+
     def _flush_loop(self):
         while True:
-            time.sleep(0.05)
-            now = _now_milliseconds()
-            buf_len = len(self.__buffer)
-            if buf_len == 0:
-                continue
-            
-            if buf_len >= SAMPLED_BUFFER_FLUSH_SIZE:
-                self._do_flush()
+            try:
+                item = self.__q.get(timeout=0.05)
+                self.__buffer += json.dumps(item).replace("\r", " ").encode()
+                self.__buffer += b"\r"
+                self.__buffer_message_count += 1
+                self.__q.task_done()
+            except queue.Empty:
+                pass
+
+            if self.__buffer_message_count == 0:
                 continue
 
-            if now >= (self.__last_flush + SAMPLED_BUFFER_FLUSH_INTERVAL_MS):
+            at_flush_size = self.__buffer_message_count >= SAMPLED_BUFFER_FLUSH_SIZE
+            at_flush_time = _now_milliseconds() >= (
+                self.__last_flush + SAMPLED_BUFFER_FLUSH_INTERVAL_MS
+            )
+
+            if at_flush_size or at_flush_time:
                 self._do_flush()
 
     def _do_flush(self):
-        buffer, self.__buffer = self.__buffer, []
+        print(f"flushing {self.__buffer_message_count} samples")
+        buffer, self.__buffer = self.__buffer, b""
+        self.__buffer_message_count = 0
         self.__last_flush = _now_milliseconds()
-        output = SAMPLED_MESSAGE_SEPARATOR.join(json.dumps(s) for s in buffer)
 
         attempt = 1
-        sock = None
         while attempt <= 3:
             try:
-                sock = socket.create_connection((SAMPLED_DAEMON_HOST, SAMPLED_DAEMON_PORT))
-                sock.sendall(output.encode())
-                sock.close()
+                if not self.__socket:
+                    self.__socket = socket.create_connection(
+                        (SAMPLED_DAEMON_HOST, SAMPLED_DAEMON_PORT)
+                    )
+                self.__socket.sendall(buffer)
                 return
             except Exception as e:
-                if sock is not None:
-                    sock.close()
+                if self.__socket is not None:
+                    self.__socket.close()
+                    self.__socket = None
                 print(f"Failed to push samples, attempt: {attempt}, error: {e}")
-                attempt += 1
                 time.sleep(1)
+                attempt += 1
 
-    def add_sample(self, dataset: str, sample: Dict[str, any]):
-        self.__buffer.append({
-            "timestamp": _now_milliseconds(),
-            "dataset": dataset,
-            "sample": sample
-        })
+    def add_sample(self, dataset: str, sample: Dict[str, FieldType]):
+        try:
+            self.__q.put(
+                item={
+                    "timestamp": _now_milliseconds(),
+                    "dataset": dataset,
+                    "sample": sample,
+                },
+                block=False,
+            )
+        except queue.Full:
+            # drop incoming samples if the queue is full
+            # a full queue means the publisher thread is broken
+            pass
 
 
-sample_buffer = SampleBuffer()
+_sample_buffer = SampleBuffer()
 
 
 def add_sample(dataset: str, sample: Dict[str, FieldType]):
-    sample_buffer.add_sample(dataset, sample)
+    _sample_buffer.add_sample(dataset, sample)
